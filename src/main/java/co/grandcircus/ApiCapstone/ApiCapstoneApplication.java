c25c48e5722c810056005ef11c44afae2adc76bd
David <dawatkins16@gmail.com>
2019-08-08 11:07:15 -0400

    initial commit

---
+++ src/main/java/co/grandcircus/ApiCapstone/ApiCapstoneApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.ApiCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCapstoneApplication.class, args);
+	}
+
+}
