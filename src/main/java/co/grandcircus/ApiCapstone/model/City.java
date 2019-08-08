5e167b4eacc02b4080d66a7be44d1cbbf78cfc50
E Jackie Zablah <jzablah@gmail.com>
2019-08-08 11:52:51 -0400

    city and state classes

--- src/main/java/co/grandcircus/ApiCapstone/model/City.java
+++ src/main/java/co/grandcircus/ApiCapstone/model/City.java
@@ -1,5 +1,13 @@
 package co.grandcircus.ApiCapstone.model;
 
 public class City {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/co/grandcircus/ApiCapstone/model/State.java
+++ src/main/java/co/grandcircus/ApiCapstone/model/State.java
@@ -1,5 +1,13 @@
 package co.grandcircus.ApiCapstone.model;
 
 public class State {
+ private String name;
 
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
 }
